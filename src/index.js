517c7b88211acbaa1a005d615b048fe561fcaef8
kawaguchi1102 <info@kawaguchi-design.com>
2024-09-26 00:15:22 +0900

    Update: Refactoring

--- src/index.js
+++ src/index.js
@@ -14,36 +14,35 @@ const meta = {
     url: "https://github.com/kawaguchi1102/stylelint-letter-spacing-negative-limit#readme"
 };
 
-const rule = primary => {
-    return (root, result) => {
-        const validOptions = validateOptions(result, ruleName, {
-            actual: primary,
-            possible: [true]
-        });
+/**@type {import('stylelint').StylelintPlugin} */
+const rule = (primary) => (root, result) => {
+    const validOptions = validateOptions(result, ruleName, {
+        actual: primary,
+        possible: [true]
+    });
 
-        if (!validOptions) return;
+    if (!validOptions) return;
 
-        root.walkDecls("letter-spacing", decl => {
-            const { value } = decl;
-            valueParser(value).walk(node => {
-                const {value, type} = node;
-                if (type === "word") {
-                    const numberUnit = valueParser.unit(value);
-                    if (numberUnit === false) return;
-                    const { number } = numberUnit;
+    root.walkDecls("letter-spacing", decl => {
+        const parsedValue = valueParser(decl.value);
+        parsedValue.walk(node => {
+            const {value, type} = node;
+            if (type === "word") {
+                const numberUnit = valueParser.unit(value);
+                if (!numberUnit) return;
 
-                    if (Number.parseFloat(number) < 0) {
-                        report({
-                            message: messages.expected(value),
-                            node: decl,
-                            result,
-                            ruleName,
-                        });
-                    }
+                const number = Number.parseFloat(numberUnit.number);
+                if (number < 0) {
+                    report({
+                        message: messages.expected(decl.value),
+                        node: decl,
+                        result,
+                        ruleName,
+                    });
                 }
-            })
+            }
         });
-    };
+    });
 };
 
 rule.ruleName = ruleName;
